617bf4efe87fe03a4907a14da76fb382b3335d90
MARVIN <o.silverygold@gmail.com>
2023-07-12 17:24:13 +0300

    Update author input

--- src/components/addBook.js
+++ src/components/addBook.js
@@ -6,11 +6,7 @@ const AddBook = ({ addNewBook }) => (
     <h3 className={add.heading}>ADD NEW BOOK</h3>
     <form className={add.form}>
       <input type="text" placeholder="Book Title" className={add.input} />
-      <select name="category" id="category" className={add.select}>
-        <option value="Action">Action</option>
-        <option value="Science Fiction">Science Fiction</option>
-        <option value="Economy">Economy</option>
-      </select>
+      <input type="text" placeholder="Author" className={add.input} />
       <button type="submit" className={add.submit} onClick={addNewBook}>ADD BOOK</button>
     </form>
   </div>
